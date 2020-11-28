0d3905b60bac60a3439b8489ba397d78bda8cd8a
rullec <1009694460@qq.com>
2020-11-28 16:06:58 +0800

    fit new PD coef

--- src/BulletGenDynamics/btGenController/PDController/btGenPDController.cpp
+++ src/BulletGenDynamics/btGenController/PDController/btGenPDController.cpp
@@ -227,7 +227,7 @@ void btGenPDController::ParseConfig(const std::string &string)
     // init_target_q << 0, 0.75, 0, 0, 0, 0, -1.09805, 0, 0, 1.6, 0, 0, 0, 0, 0, 0,
     //     0, 0, 0, 0;
     printf("[error] the init target is set to random now!\n");
-    init_target_q.setZero();
+    init_target_q.setRandom();
     init_target_qdot.setZero();
     std::cout << "[pd] init q target = " << init_target_q.transpose()
               << std::endl;
